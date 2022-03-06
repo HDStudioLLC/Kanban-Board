a40f493540cc229dbf1d056078141e6e2872b977
Elizabeth <Hindash@email.arizona.edu>
2022-03-06 15:48:10 -0700

    bug found, it was the task ID preventing it from catching the task

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,14 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
-  event.preventDefault();
-
+  // event.preventDefault();
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("click", createTaskHandler);
 
 // create list item
 var listItemEl = document.createElement("li");
